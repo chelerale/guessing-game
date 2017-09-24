8f847bde11c4b9f87db0db4e4ae51e92b2d8497c
Lera Chernenko <smeshklio@mail.ru>
2017-09-25 01:15:07 +0300

    first

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    this.a = min
+    this.b = max
     }
 
     guess() {
-
+      return Math.ceil((this.a + this.b)/2)
     }
 
     lower() {
-
+      this.b = Math.ceil((this.a + this.b)/2)
     }
 
     greater() {
-
+      this.a = Math.ceil((this.a + this.b)/2)
     }
 }
 
